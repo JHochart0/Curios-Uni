33c6cdeccd3b474f951b4c2298d7ea6aa02fba3a
Jovany Hochart <jovanyhochart@gmail.com>
2024-06-27 13:42:08 +0200

    filling the initialisation of the project + start of authentification system

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,26 @@
+// all libraries requires
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
-app.use(express.static('public'));
+// routes requires
+const authRoutes = require('./routes/authRoutes');
 
+// express instanciation
 const app = express();
 
+// view engine
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+//middlewares and static files
+app.use(express.static('public'));
+app.use(express.json());
+
+//init mongoose
+require("./controllers/mongoose_init");
+
+// Routing
+app.use('/', authRoutes);
+
+module.exports = app;
---
+++ backend/controllers/authController.js
@@ -0,0 +1,70 @@
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: './.env'} );
+
+// create jwt token
+const maxAge = 1*24*60*60; //1 day in seconds
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.SECRET_JWT, {
+        expiresIn:maxAge
+    });
+}
+
+// handle signup and login errors (NEED TO COMPLETE IT WHEN I FINISH THE USER MODEL)
+const handleErrors = (err) =>{
+    let errors = {email: '', password: ''};
+
+    //incorrect email or password during the login
+    if(err.message=='incorrect login'){
+        errors.password = 'Incorrect email or password. Try again.'
+    }
+
+    //duplicated email error code during registration
+    if(err.code===11000){
+        errors.email = 'That email is already registered.'
+        return errors;
+    }
+
+    //validation errors
+    if (err.message.includes('User validation failed')){
+        Object.values(err.errors).forEach( ({properties}) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+}
+
+module.exports.signup_get = (req, res)=>{
+    res.send('signup get');
+}
+
+module.exports.signup_post = async (req, res)=>{
+    const {email, password} = req.body;
+    try{
+        const user = await User.create({ email, password });
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000 });
+        res.status(201).json({user: user._id});
+    }
+    catch(err){
+        const errors = handleErrors(err);
+        res.status(400).json({errors});
+    }
+}
+
+
+module.exports.login_get = (req, res)=>{
+    res.send('login get');
+
+}
+
+module.exports.login_post = async (req, res)=>{
+    res.send('login post');
+
+}
+
+module.exports.logout = (req, res)=>{
+    res.cookie('jwt', '', {maxAge: 1});
+    res.redirect('/');
+}
